d48b3ed5a379ad4107f3a03db0049b607bb5f3e8
Andrsss122 <104798449+Andrsss122@users.noreply.github.com>
2022-07-14 15:41:33 -0600

    salidas cambio

---
+++ swagger_jwt/Models/Salidas.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace swagger_jwt.Models
+{
+    public class Salidas
+    {
+
+        [Key]
+        public int EntradaId { get; set; }
+        public int ProductoId { get; set; }
+
+        public int BodegaId { get; set; }
+
+        public int UsuarioId { get; set; }
+
+        public DateTime FechaEntrada { get; set; }
+
+        public float Cantidad { get; set; }
+
+        public float EntradaTotal { get; set; }
+
+        public Producto producto { get; set; }
+        public Bodega bodega { get; set; }
+        public Usuario usuario { get; set; }
+        public Inventario inventario { get; set; }
+
+    }
+}
