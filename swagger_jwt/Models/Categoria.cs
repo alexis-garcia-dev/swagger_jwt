70f309c4e6340e544389140b43019397533e749a
Andrsss122 <104798449+Andrsss122@users.noreply.github.com>
2022-07-14 14:33:03 -0600

    Entradas modelo, categoria modelo, fis some issues en productos

---
+++ swagger_jwt/Models/Categoria.cs
@@ -0,0 +1,14 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace swagger_jwt.Models
+{
+    public class Categoria
+    {
+        [Key]
+        public int CategoriaId { get; set; }
+
+
+
+    }
+}
---
+++ swagger_jwt/Models/Entradas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace swagger_jwt.Models
+{
+    public class Entradas
+    {
+
+        [Key]
+        public int EntradaId { get; set; }
+        public int ProductoId { get; set; }
+
+        public int BodegaId { get; set; }
+
+        public int UsuarioId { get; set; }
+
+        public DateTime FechaEntrada { get; set; }
+
+        public float Cantidad { get; set; }
+
+        public float EntradaTotal { get; set; }
+
+        public Producto producto { get; set; }
+        public Bodega bodega { get; set }
+        public Usuario usuario { get; set; }
+
+    }
+}
